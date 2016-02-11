16f26b7524ac460f67307dd57ed6f96a2c469432
Lucksika Losuvalna <joiiremy@gmail.com>
2016-02-11 16:53:25 +0700

    Fix bug game pause #1

--- app/src/main/java/pl/engine/GameShakeEngine.java
+++ app/src/main/java/pl/engine/GameShakeEngine.java
@@ -38,6 +38,7 @@ public class GameShakeEngine extends GameEngine{
 			}
 		}
 		if(event.equals("game_pause")) {
+			sendGameEventToClient("game_pause", new String[]{});
 			gc.getGameLister().onIncommingEvent("game_pause", new String[]{});
 			gameManager.stopTimer();
 			gamePaused = true;
